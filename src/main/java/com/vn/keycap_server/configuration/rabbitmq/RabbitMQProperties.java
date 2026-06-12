241d1d1f59403903d0dff1596aeb55916112986d
huuvan <huuvan2005vn@gmail.com>
2026-06-12 15:49:07 +0700

    fix (rabbitmq): fix RabbitMQProperties.java and application.yaml

--- src/main/java/com/vn/keycap_server/configuration/rabbitmq/RabbitMQProperties.java
+++ src/main/java/com/vn/keycap_server/configuration/rabbitmq/RabbitMQProperties.java
@@ -1,13 +1,15 @@
 package com.vn.keycap_server.configuration.rabbitmq;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@ConfigurationProperties(prefix = "order")
+@Component
+@ConfigurationProperties(prefix = "order.payment")
 public class RabbitMQProperties {
 
     private int expiryMinutes;
