0f9259bc1af4b8e1a2f48b209bc06605c1390351
HauPhan <phuchau.nam2005@gmail.com>
2026-06-19 22:06:04 +0700

    add: preauthorize for api controller admin and staff

--- src/main/java/com/vn/keycap_server/controller/admin/AdminOrderController.java
+++ src/main/java/com/vn/keycap_server/controller/admin/AdminOrderController.java
@@ -12,6 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -21,63 +22,66 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/orders")
 public class AdminOrderController {
 
-    private final IAdminOrderService adminOrderService;
+        private final IAdminOrderService adminOrderService;
 
-    @GetMapping()
-    public ResponseEntity<ApiResponse> getOrders(
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "20") int limit,
-            @RequestParam(required = false) String keyword,
-            @RequestParam(required = false) EOrderStatus status
-    ) {
+        @GetMapping()
+        @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_STAFF')")
+        public ResponseEntity<ApiResponse> getOrders(
+                        @RequestParam(defaultValue = "1") int page,
+                        @RequestParam(defaultValue = "20") int limit,
+                        @RequestParam(required = false) String keyword,
+                        @RequestParam(required = false) EOrderStatus status) {
 
-        Page<OrderAdminResponse> resultPage = adminOrderService.getOrders(page, limit, keyword, status);
-        PaginationMeta meta = PaginationUtils.buildPaginationMeta(resultPage, page);
-        return ResponseEntity.ok(ApiResponse.builder()
-                .success(true)
-                .message("Get orders for admin success")
-                .data(resultPage.getContent())
-                .pagination(meta)
-                .build());
-    }
+                Page<OrderAdminResponse> resultPage = adminOrderService.getOrders(page, limit, keyword, status);
+                PaginationMeta meta = PaginationUtils.buildPaginationMeta(resultPage, page);
+                return ResponseEntity.ok(ApiResponse.builder()
+                                .success(true)
+                                .message("Get orders for admin success")
+                                .data(resultPage.getContent())
+                                .pagination(meta)
+                                .build());
+        }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse> getOrder(@PathVariable int id) {
-        return ResponseEntity.ok(ApiResponse.builder()
-                .success(true)
-                .message("Lấy chi tiết đơn hàng thành công")
-                .data(adminOrderService.getOrderDetail(id))
-                .build());
-    }
+        @GetMapping("/{id}")
+        @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_STAFF')")
+        public ResponseEntity<ApiResponse> getOrder(@PathVariable int id) {
+                return ResponseEntity.ok(ApiResponse.builder()
+                                .success(true)
+                                .message("Lấy chi tiết đơn hàng thành công")
+                                .data(adminOrderService.getOrderDetail(id))
+                                .build());
+        }
 
-    // Update Status Order
-    @PatchMapping("/{id}/status")
-    public ResponseEntity<ApiResponse> updateOrderStatus(
-            @PathVariable int id,
-            @RequestBody @Valid UpdateOrderStatusRequest request,
-            @AuthenticationPrincipal Jwt jwt) {
+        // Update Status Order
+        @PatchMapping("/{id}/status")
+        @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_STAFF')")
+        public ResponseEntity<ApiResponse> updateOrderStatus(
+                        @PathVariable int id,
+                        @RequestBody @Valid UpdateOrderStatusRequest request,
+                        @AuthenticationPrincipal Jwt jwt) {
 
-        Long userId = jwt.getClaim("userId");
-        return ResponseEntity.ok(ApiResponse.builder()
-                .success(true)
-                .message("Cập nhật trạng thái đơn hàng thành công")
-                .data(adminOrderService.updateOrderStatus(id, request, userId))
-                .build());
+                Long userId = jwt.getClaim("userId");
+                return ResponseEntity.ok(ApiResponse.builder()
+                                .success(true)
+                                .message("Cập nhật trạng thái đơn hàng thành công")
+                                .data(adminOrderService.updateOrderStatus(id, request, userId))
+                                .build());
 
-    }
+        }
 
-    // Cancel Order
-    @PatchMapping("/{id}/cancel")
-    public ResponseEntity<ApiResponse> cancelOrder(
-            @PathVariable Long id,
-            @RequestBody @Valid CancelOrderRequest request,
-            @AuthenticationPrincipal Jwt jwt) {
-        Long adminId = jwt.getClaim("userId");
-        return ResponseEntity.ok(ApiResponse.builder()
-                .success(true)
-                .message("Đơn hàng đã được hủy thành công")
-                .data(adminOrderService.cancelOrder(id, request, adminId))
-                .build());
-    }
+        // Cancel Order
+        @PatchMapping("/{id}/cancel")
+        @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_STAFF')")
+        public ResponseEntity<ApiResponse> cancelOrder(
+                        @PathVariable Long id,
+                        @RequestBody @Valid CancelOrderRequest request,
+                        @AuthenticationPrincipal Jwt jwt) {
+                Long adminId = jwt.getClaim("userId");
+                return ResponseEntity.ok(ApiResponse.builder()
+                                .success(true)
+                                .message("Đơn hàng đã được hủy thành công")
+                                .data(adminOrderService.cancelOrder(id, request, adminId))
+                                .build());
+        }
 
 }
