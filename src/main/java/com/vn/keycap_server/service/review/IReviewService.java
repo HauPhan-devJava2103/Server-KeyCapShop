5e8daa6580d83e3c189d396d6a076abaf07459c4
chuonghoai <144915081+chuonghoai@users.noreply.github.com>
2026-06-23 10:27:07 +0700

    feat: add API get available review in older order

--- src/main/java/com/vn/keycap_server/service/review/IReviewService.java
+++ src/main/java/com/vn/keycap_server/service/review/IReviewService.java
@@ -5,6 +5,8 @@ import com.vn.keycap_server.dto.request.review.CreateReviewRequest;
 import org.springframework.data.domain.Page;
 
 import com.vn.keycap_server.dto.response.review.ReviewResponse;
+import com.vn.keycap_server.dto.response.review.AvailableReviewResponse;
+import java.util.List;
 
 public interface IReviewService {
     Page<ReviewResponse> getReviewsByProductId(Long productId, int page, int pageSize);
@@ -12,4 +14,6 @@ public interface IReviewService {
     void createReviews(CreateReviewRequest request, Long userId);
 
     void replyToReview(Long reviewId, CreateReplyRequest request, Long userId);
+
+    List<AvailableReviewResponse> getAvailableReviews(Long orderId, Long userId);
 }
