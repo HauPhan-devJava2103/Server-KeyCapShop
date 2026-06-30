f62e4cc54dd1a04d7e6c568ec35e8beb172a35cb
HauPhan <phuchau.nam2005@gmail.com>
2026-06-30 22:21:01 +0700

    fix: new object factory method

--- src/main/java/com/vn/keycap_server/service/auth/login/LoginFactoryRegistry.java
+++ src/main/java/com/vn/keycap_server/service/auth/login/LoginFactoryRegistry.java
@@ -1,35 +1,29 @@
 package com.vn.keycap_server.service.auth.login;
 
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Component;
 
-import com.vn.keycap_server.exception.BadRequestException;
 import com.vn.keycap_server.service.auth.login.creator.AbstractLoginCreator;
+import com.vn.keycap_server.service.auth.login.creator.BasicLoginCreator;
+import com.vn.keycap_server.service.auth.login.creator.GoogleLoginCreator;
+import com.vn.keycap_server.service.auth.login.product.BasicLoginHandler;
+import com.vn.keycap_server.service.auth.login.product.GoogleLoginHandler;
 import com.vn.keycap_server.utils.ELoginType;
 
 @Component
 public class LoginFactoryRegistry {
+    private final BasicLoginHandler basicLoginHandler;
+    private final GoogleLoginHandler googleLoginHandler;
 
-    private final Map<ELoginType, AbstractLoginCreator> creatorMap;
-
-    public LoginFactoryRegistry(List<AbstractLoginCreator> creators) {
-        this.creatorMap = creators.stream()
-                .collect(Collectors.toMap(
-                        AbstractLoginCreator::getLoginType,
-                        Function.identity()));
+    public LoginFactoryRegistry(BasicLoginHandler basicLoginHandler, GoogleLoginHandler googleLoginHandler) {
+        this.basicLoginHandler = basicLoginHandler;
+        this.googleLoginHandler = googleLoginHandler;
     }
 
     public AbstractLoginCreator getCreator(ELoginType loginType) {
-        AbstractLoginCreator creator = creatorMap.get(loginType);
-        if (creator == null) {
-            throw new BadRequestException(
-                    "Phương thức đăng nhập '" + loginType + "' không được hỗ trợ.");
-        }
-        return creator;
+        return switch (loginType) {
+            case BASIC -> new BasicLoginCreator(basicLoginHandler);
+            case GOOGLE -> new GoogleLoginCreator(googleLoginHandler);
+        };
     }
 
 }
